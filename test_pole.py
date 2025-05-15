34f00439fbd10e58b86adee2fa15a9c49ab86c98
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2025-05-15 15:54:23 +0800

    额....3d?

--- test_pole.py
+++ test_pole.py
@@ -1,35 +1,44 @@
 from manimlib import *
 import numpy as np
 
-
+# 添加球坐标转换函数
+def spherical_to_cartesian(r, phi, theta):
+    """球坐标转笛卡尔坐标
+    Args:
+        r: 半径
+        phi: 方位角 (绕z轴旋转，0-2π)
+        theta: 极角 (与z轴夹角，0-π)
+    """
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+    return np.array([x, y, z])
 
 class magnet(Scene):
     def construct(self):
-        circle = Circle()
-        circle.set_fill(BLUE, opacity=1)
-        circle.set_stroke(WHITE, width=4)
-        square = Square()
-
-        self.play(ShowCreation(square))
-        # self.play(ShowCreation(circle))
-        # self.wait(5)
-        #
-        # self.embed()
-
-
-        self.play(ReplacementTransform(square, circle))
+        # 创建左右两个点
+        left_dot = Dot(LEFT * 3)
+        right_dot = Dot(RIGHT * 3)
+        
+        # 添加放射光线（数量可调）
+        num_rays = 120  # 将数量从12大幅增加到48
+        
+        def create_rays(dot, num):
+            rays = VGroup()
+            for i in range(num):
+                angle = 2 * PI * i / num
+                # 使用球坐标系生成3D方向
+                dir = spherical_to_cartesian(1, angle, PI/4)
+                # 修改光线长度为5倍方向向量（原为0.5）
+                ray = Line(dot.get_center(), dot.get_center() + dir * 5)
+                ray.set_stroke(WHITE, width=1)
+                rays.add(ray)
+            return rays
+        
+        left_rays = create_rays(left_dot, num_rays)
+        right_rays = create_rays(right_dot, num_rays)
+        
+        # 显示两个点和光线
+        self.play(ShowCreation(left_dot), ShowCreation(right_dot))
+        self.play(ShowCreation(left_rays), ShowCreation(right_rays))
         self.wait()
-        self.play(circle.animate.stretch(4, 0))
-        self.play(Rotate(circle, 90 * DEG))
-        self.play(circle.animate.shift(2 * RIGHT).scale(0.25))
-
-        text = Text("""
-            In general, using the interactive shell
-            is very helpful when developing new scenes
-        """)
-        self.play(Write(text))
-
-
-        always(circle.move_to, self.mouse_point)
-
-
