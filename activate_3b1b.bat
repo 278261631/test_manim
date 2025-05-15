ee1ca2f4c814312681df7013d7c4a28d7eefedfd
mars <mars.maya.g@gmail.com>
2025-05-15 13:23:06 +0800

    Add files via upload

---
+++ activate_3b1b.bat
@@ -0,0 +1,20 @@
+cd /d E:/3b1b
+rem C:/Python/Python310/python.exe -m venv 3b1b_env
+3b1b_env\Scripts\activate
+
+cd /d E:/3b1b/manim-master
+
+manimgl example_scenes.py OpeningManimExample
+manimgl example_scenes.py AnimatingMethods
+manimgl example_scenes.py TextExample
+manimgl example_scenes.py TexTransformExample
+manimgl example_scenes.py TexIndexing
+manimgl example_scenes.py UpdatersExample
+manimgl example_scenes.py CoordinateSystemExample
+manimgl example_scenes.py GraphExample
+manimgl example_scenes.py TexAndNumbersExample
+manimgl example_scenes.py SurfaceExample
+manimgl example_scenes.py InteractiveDevelopment
+manimgl example_scenes.py ControlsExample
+
+
